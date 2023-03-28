0e45db110555ed4e29f8d1825fd3c41159b1ac8f
Rebekah Callari-Kaczmarczyk <rebekahcallari@gmail.com>
2023-03-28 18:54:22 -0400

    add prompt for demo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 function App() {
+  // What if we wanted to keep track of how many times the button was clicked?
   const handleClick = () => {
     console.log("this button was clicked");
   };
