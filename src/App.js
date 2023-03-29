ec0f3d638bda6b40325e2911b8568cf4d4875e7e
Rebekah Callari-Kaczmarczyk <rebekahcallari@gmail.com>
2023-03-28 20:01:11 -0400

    comments

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import { useState } from "react";
+import Button from "./Button";
 
 function App() {
+  // Manage the state of the click count (default value is 0 on initial render)
+  // needs to be defined here because we need access to it for the <p> element
   const [clickCount, setClickCount] = useState(0);
-  // What if we wanted to keep track of how many times the button was clicked?
-  const handleClick = () => {
-    const newCount = clickCount + 1;
-    setClickCount(newCount);
-    console.log("this button was clicked");
-  };
+
   return (
     <div>
-      <button onClick={handleClick}>Click Me!</button>
+      <Button clickCount={clickCount} setClickCount={setClickCount}>
+        Click Me!
+      </Button>
       <p>{`This button was clicked ${clickCount} times.`}</p>
     </div>
   );
