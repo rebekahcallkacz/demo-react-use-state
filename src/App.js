a87b29f8f77602b6152099232fd127d15dcef43b
Rebekah Callari-Kaczmarczyk <rebekahcallari@gmail.com>
2023-03-28 20:00:39 -0400

    add button component and keep track of clicks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from "react";
+import { useState } from "react";
 
 function App() {
+  const [clickCount, setClickCount] = useState(0);
   // What if we wanted to keep track of how many times the button was clicked?
   const handleClick = () => {
+    const newCount = clickCount + 1;
+    setClickCount(newCount);
     console.log("this button was clicked");
   };
   return (
     <div>
       <button onClick={handleClick}>Click Me!</button>
+      <p>{`This button was clicked ${clickCount} times.`}</p>
     </div>
   );
 }
